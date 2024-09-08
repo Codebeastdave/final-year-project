06e08e74c0ee79a66e2fea5b6269435c290003b2
muraina david <muraina david>
2024-09-08 21:36:31 +0100

    new question model added

--- mywarmupapp/views.py
+++ mywarmupapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(response)
+def index(response):
     return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.

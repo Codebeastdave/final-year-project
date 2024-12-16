0cf8220378a5ab6bc84dc737da93178fbb24c2a9
david <codebeastdave@gmail.com>
2024-12-17 00:35:44 +0100

    project initialization

--- myfinalyearproject/urls.py
+++ myfinalyearproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("mywarmupapp/", include("mywarmupapp.urls")),
+    path("", include("mywarmupapp.urls")),
 ]

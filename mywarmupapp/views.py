743e2a1bfac61f8ad39aa2771c2d7e2f18a65c3f
david <codebeastdave@gmail.com>
2025-03-29 18:18:29 +0100

    updated vote function to include a try/except/else clause, so if someone doesint vote but submits the person will be redirected back to the voting page

--- mywarmupapp/views.py
+++ mywarmupapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 def index(request):
@@ -6,10 +6,7 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "mywarmupapp/index.html", context)
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "mywarmupapp/detail.html", {"question": question})
 
 def results(request, question_id):
@@ -18,5 +15,13 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-# Create your views here.
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice.set.get(pk = request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, "mywarmupapp/detail.html",{"question":question, "error": "you did'nt choose any option"},)
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("mywarmupapp:results", args = (question.id,)))
+     #Create your views here.

0cb84f8dd0c2b47bb46f31932c4bdb469a32934c
david <codebeastdave@gmail.com>
2025-03-30 23:15:19 +0100

    changed all instances of as_view to as_view()

--- mywarmupapp/urls.py
+++ mywarmupapp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 app_name = "mywarmupapp"
 urlpatterns = [
-    path("", views.IndexView.as_view, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.DetailView.as_view, name="detail"),
+    path("<int:question_id>/", views.DetailView.as_view(), name="detail"),
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.ResultView.as_view, name="results"),
+    path("<int:question_id>/results/", views.ResultView.as_view(), name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]

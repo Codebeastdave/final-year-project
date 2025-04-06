00001248d54c225ee95c8aa068a6616a106c9d84
david <codebeastdave@gmail.com>
2025-04-06 10:48:43 +0100

    registered both question and choice models

--- mywarmupapp/admin.py
+++ mywarmupapp/admin.py
@@ -1,10 +1,4 @@
-from django.http import HttpResponse
-
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
+from django.contrib import admin
+from .models import Question, Choice
+admin.site.register(Question)
+admin.site.register(Choice)

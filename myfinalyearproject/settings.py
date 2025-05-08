4bdee48fadaf6980b5852aaf1cd9ce0250f473cd
Codebeastdave <murainadavidabisoye@gmail.com>
2025-05-08 12:35:48 +0000

    secret key is now an env variable

--- myfinalyearproject/settings.py
+++ myfinalyearproject/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("../../mydjangokey.pub", "r") as text:
-    SECRET_KEY = text.read()
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['2.4', '127.0.0.1','192.168.43.90']
+ALLOWED_HOSTS = ['2.4', '127.0.0.1','192.168.43.90','dave965.pythonanywhere.com']
 
 
 # Application definition

1d6f4a7acb5a18ca5cffd8437950bfcec3c6f622
Codebeastdave <murainadavidabisoye@gmail.com>
2025-05-15 21:14:37 +0000

    secret keys are npw properly encrypted

--- myfinalyearproject/settings.py
+++ myfinalyearproject/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+print(SECRET_KEY, 98076)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -122,5 +122,5 @@ STATIC_URL ="/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"staticfiles")]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

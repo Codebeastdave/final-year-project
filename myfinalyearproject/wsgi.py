a02b6d048517cd8634849f1bc5c62d0674567daf
muraina david <muraina david>
2026-07-05 11:28:01 +0100

    edited wsgi file to fit vercel build configurations

--- myfinalyearproject/wsgi.py
+++ myfinalyearproject/wsgi.py
@@ -11,11 +11,12 @@ import os
 import sys
 
 # add your project directory to the sys.path
-project_home = '/home/dave965/myfirstproject/my-final-year-project'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+#project_home = '/home/dave965/myfirstproject/my-final-year-project'
+#if project_home not in sys.path:
+#    sys.path.insert(0, project_home)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfinalyearproject.settings')
 
 application = get_wsgi_application()
+app = application
